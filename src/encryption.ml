960e877b828a2a906b2e4a509fd7c8cf810226a7
WellDN <Wellingtontp0@gmail.com>
2023-08-14 19:19:18 -0300

    encrypted key on file key

--- src/encryption.ml
+++ src/encryption.ml
@@ -23,41 +23,48 @@ let rnd_key() =
     done;
     Bytes.to_string key
 
-module Encrypt = struct
-   let encrypt input_file =
-       let plaintext = read_file input_file in
-       let key = rnd_key() in
-       let encrypted_text = xor_cipher key plaintext in
-       write_file input_file encrypted_text;
+let encrypt input_file =
+    let plaintext = read_file input_file in
+    let key = rnd_key() in
+    let encrypted_text = xor_cipher key plaintext in
+    let key_file = input_file ^ ".key.txt" in
+    write_file input_file encrypted_text;
+    write_file key_file key;
        key
-end
 
-module Decrypt = struct
-   let decrypt input_file key =
-       let plaintext = read_file input_file in
-       let decrypted_text = xor_cipher key plaintext in
-       write_file input_file decrypted_text
-end
+let decrypt input_file key_file =
+    let key = read_file key_file in
+    let plaintext = read_file input_file in
+    let decrypted_text = xor_cipher key plaintext in
+    write_file input_file decrypted_text
 
    let () =
-       if Array.length Sys.argv <> 4 then (
-           Printf.eprintf "Usage: %s <encrypt/decrypt> <inputfile> <key>\n" Sys.argv.(0);
+       if Array.length Sys.argv < 3 || Array.length Sys.argv > 4 then (
+           Format.eprintf "Usage: %s <encrypt/decrypt> <inputfile> [<key>]\n" Sys.argv.(0);
     exit 1
            );
 
 let command = Sys.argv.(1) in
 let input_file = Sys.argv.(2) in
-let key = Sys.argv.(3) in
+let key_file =  
+    match command with
+    | "encrypt" -> encrypt input_file 
+    | "decrypt" -> if Array.length Sys.argv = 4 then Sys.argv.(3) else (
+        Format.eprintf "Usage: %s decrypt <inputfile> [<key_file>]\n" Sys.argv.(0);
+        exit 1
+        )
+    | _ ->
+            Format.eprintf "Unknown command: %s\n" command;
+            exit 1
+in
 
 match command with
 | "encrypt" ->
-        let encryption_key = Encrypt.encrypt input_file in
-        Printf.printf "File %s was encrypted successfully.\n" input_file;
-    Printf.printf "Secret key: %s\n" encryption_key
-| "decrypt" ->
-        Decrypt.decrypt input_file key;
-    Printf.printf "File %s was decrypted successfully.\n" input_file
-| _ ->
-        Printf.eprintf "Unknown command: %s\n" command;
+        Format.printf "File %s was encrypted successfully.\n" input_file;
+    Format.printf "To decrypt the file %s: \n dune exec ./encryption/src/encryption.exe decrypt filename filename.key.txt\n" input_file;
+    | "decrypt" ->
+            decrypt input_file key_file;
+    Format.printf "File %s was decrypted successfully.\n" input_file;
+    | _ ->
+            Format.eprintf "Unknown command: %s\n" command;
     exit 1
-
